2494210aabec58ea5671ccc98124fcddaed0e925
Alex Wang <alexwzy88@gmail.com>
2022-10-13 17:37:44 -0400

    update files

--- Github.py
+++ Github.py
@@ -5,12 +5,12 @@ import yaml
 # credentials.yml contains your usr/repo and PAT created in step 11 above
 # So we load the data into a YML object
 
-data = yaml.safe_load(open('credentials.yml'))
+data = yaml.safe_load(open('username-credentials.yml'))
 
 # Extract the user and token from the data object
 # 0. Complete these 2 lines below
-user = 'Alex-Wang-88/devops'
-token = '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'
+user = data["creds"]["username"]
+token = data["creds"]["token"]
 
 # using an access token
 g = Github(token)
@@ -19,7 +19,7 @@ repo = g.get_repo(user)
 ## Complete your tasks from here
 
 # 1. Get all branches you have created for your public repo
-list(repo.get_branches())
+branches = list(repo.get_branches())
 
 # 2. Get all pull requests you have created
 pulls = repo.get_pulls(state='open', sort='created', base='master')
@@ -27,6 +27,5 @@ for pr in pulls:
     print(pr.number)
 
 # 3. Get a list of commits you have created in your `main` branch.
-#commit.getStatuses()
-#commit = repo.get_commit(sha=sha)
-#print(commit.commit.author.date)
+commit = repo.get_commits(sha=sha)
+print(commit.commit.author.date)
